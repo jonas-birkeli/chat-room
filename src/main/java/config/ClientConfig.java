caf905f4049caf6b67935c9470f7c7174da20d60
Jonas Birkeli <jonabi@stud.ntnu.no>
2024-06-09 11:04:49 +0200

    Configuration constants added

---
+++ src/main/java/config/ClientConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+/**
+ * Configuration constants for the client.
+ *
+ * @version 1.0
+ * @author Jonas Birkeli
+ * @since 08.06.2024
+ */
+public class ClientConfig {
+  public static final String USERNAME_NOT_SET = "DEFAULT_USER";
+
+  private ClientConfig() {} // Prevent instantiation
+}
---
+++ src/main/java/config/ConnectionConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+/**
+ * Configuration constants for the connection.
+ *
+ * @version 1.0
+ * @author Jonas Birkeli
+ * @since 08.06.2024
+ */
+public class ConnectionConfig {
+  public static final int PORT = 8687;
+  public static final String LOCALHOST = "127.0.0.1";
+  public static final String SERVER_HOST = "some address";
+
+  private ConnectionConfig() {} // Prevent instantiation
+}
